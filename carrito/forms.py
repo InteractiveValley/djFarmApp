a37f264576c14fa84670fe918c4432c7cd4fb1fe
richpolis <richpolis@gmail.com>
2016-04-25 16:36:39 +0000

    cambiar aspecto de formulario

--- carrito/forms.py
+++ carrito/forms.py
@@ -12,3 +12,12 @@ class ReceiptForm(forms.ModelForm):
             # Use localization and bootstrap 3
             'date_expiration': DateWidget(attrs={'id': "date_expiration"}, usel10n=True, bootstrap_version=3)
         }
+	
+	def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields:
+            # Recorremos todos los campos del modelo para añadirle class="form-control
+            self.fields[field].widget.attrs.update({'class': 'form-control'})
+
+        # Añadir atributos personalizados a campos sueltos.
+        self.fields['date_expiration'].widget.attrs.update({'placeholder': 'Fecha en que caduca'})
