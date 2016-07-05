1dddf45251bab0edc60841f18293af6970917818
richpolis <richpolis@gmail.com>
2016-07-05 21:32:35 +0000

    arreglo de comparacion

--- carrito/models.py
+++ carrito/models.py
@@ -255,11 +255,11 @@ class ImageSale(models.Model):
             self.folio_recipe = 0
         self.modified = timezone.localtime(timezone.now())
         if self.type_recipe == TYPE_RECIPE_NORMAL and self.folio_recipe == 0:
-            self.folio_recipe = ImageSale.objects.filter(type_recipe=TYPE_RECIPE_NORMAL).aggregate(Max('folio_recipe'))[0]
+            self.folio_recipe = ImageSale.objects.filter(type_recipe=TYPE_RECIPE_NORMAL).aggregate(Max('folio_recipe'))
             self.folio_recipe = self.folio_recipe + 1
             
         elif self.type_recipe == TYPE_RECIPE_WITH_ANTIBIOTICO and self.folio_recipe == 0:
-            self.folio_recipe = ImageSale.objects.filter(type_recipe=TYPE_RECIPE_WITH_ANTIBIOTICO).aggregate(Max('folio_recipe'))[0]
+            self.folio_recipe = ImageSale.objects.filter(type_recipe=TYPE_RECIPE_WITH_ANTIBIOTICO).aggregate(Max('folio_recipe'))
             self.folio_recipe = self.folio_recipe + 1
             
         return super(ImageSale, self).save(*args, **kwargs)
